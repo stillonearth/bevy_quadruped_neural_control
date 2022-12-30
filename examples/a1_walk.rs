a8b7378e11d8f5ae2160a11a111e934626787915
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2022-12-30 12:20:48 +0300

    up tract example

--- examples/a1_walk.rs
+++ examples/a1_walk.rs
@@ -1,3 +1,7 @@
+#![feature(allocator_api)]
+
+use std::alloc::Global;
+
 use bevy::prelude::*;
 use bevy_flycam::*;
 use bevy_mujoco::*;
@@ -5,12 +9,27 @@ use bevy_mujoco::*;
 use tract_ndarray::Array2;
 use tract_onnx::prelude::*;
 
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref MODEL: SimplePlan<TypedFact, Box<dyn TypedOp, Global>, Graph<TypedFact, Box<dyn TypedOp, Global>>> =
+        tract_onnx::onnx()
+            .model_for_path("assets/g1-forward.onnx")
+            .unwrap()
+            .with_input_fact(0, f32::fact([1, 119]).into())
+            .unwrap()
+            .into_optimized()
+            .unwrap()
+            .into_runnable()
+            .unwrap();
+}
+
 fn setup(mut commands: Commands) {
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 9000.0,
             range: 100.,
-            shadows_enabled: true,
+            shadows_enabled: false,
             ..default()
         },
         transform: Transform::from_xyz(8.0, 16.0, 8.0),
@@ -26,17 +45,6 @@ fn setup(mut commands: Commands) {
 }
 
 fn robot_control_loop(mut mujoco_resources: ResMut<MuJoCoResources>) {
-    // load onnx model
-    let model = tract_onnx::onnx()
-        .model_for_path("assets/g1-forward.onnx")
-        .unwrap()
-        .with_input_fact(0, f32::fact([1, 119]).into())
-        .unwrap()
-        .into_optimized()
-        .unwrap()
-        .into_runnable()
-        .unwrap();
-
     // prepare simulation data for the NN
     let qpos = mujoco_resources.state.qpos.clone();
     let qvel = mujoco_resources.state.qvel.clone();
@@ -65,23 +73,26 @@ fn robot_control_loop(mut mujoco_resources: ResMut<MuJoCoResources>) {
     }
 
     let input: Tensor = Array2::from_shape_vec((1, 119), input_vec).unwrap().into();
-    let result = model.run(tvec!(input.into())).unwrap();
+    let result = MODEL.run(tvec!(input.into())).unwrap();
     let actions = result[0].to_array_view::<f32>().unwrap();
 
     let mut control: Vec<f64> = vec![0.0; mujoco_resources.control.number_of_controls];
     for i in 0..mujoco_resources.control.number_of_controls {
         control[i] = actions[[0, i]] as f64;
     }
+
     mujoco_resources.control.data = control;
 }
 
 fn main() {
+    // load onnx model
+
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(MuJoCoPluginSettings {
             model_xml_path: "assets/unitree_a1/scene.xml".to_string(),
             pause_simulation: false,
-            target_fps: 300.0,
+            target_fps: 600.0,
         })
         .add_plugin(NoCameraPlayerPlugin)
         .insert_resource(MovementSettings {
--- examples/tract.rs
+++ examples/tract.rs
@@ -3,17 +3,17 @@ use tract_onnx::prelude::*;
 
 fn main() {
     let model = tract_onnx::onnx()
-        .model_for_path("assets/g1-control-direction.onnx")
+        .model_for_path("assets/g1-forward.onnx")
         .unwrap()
-        .with_input_fact(0, f32::fact([1, 121]).into())
+        .with_input_fact(0, f32::fact([1, 119]).into())
         .unwrap()
         .into_optimized()
         .unwrap()
         .into_runnable()
         .unwrap();
 
-    let zeros: Vec<f32> = (0..121).map(|_| 0.0).collect();
-    let input_array = Array2::from_shape_vec((1, 121), zeros).unwrap();
+    let zeros: Vec<f32> = (0..119).map(|_| 0.0).collect();
+    let input_array = Array2::from_shape_vec((1, 119), zeros).unwrap();
     let input: Tensor = input_array.into();
     let result = model.run(tvec!(input.into())).unwrap();
     println!(
